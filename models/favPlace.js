3b30c0cefd47b53d20cac828ab559d39e5d54577
Alina Kitaeva <akitaeva@gmail.com>
2018-08-07 09:30:52 -0400

    note routes and place model modified

--- models/favPlace.js
+++ models/favPlace.js
@@ -4,8 +4,8 @@ const Schema   = mongoose.Schema;
 const placeSchema = new Schema({
   name:        {type: String, required: true},
   category:    {type: String, required: true},
-  phoneNumber: {type: String, required: true},
-  address:     {type: String, required: true},
+  phoneNumber: {type: String},
+  address:     {type: String},
   notes:       {type: String}
   //owner:       {type: Schema.Types.ObjectId, ref: 'User'}
 }, {
